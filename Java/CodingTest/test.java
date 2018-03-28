af10b79351aafdb7bf61002d317e716ab625fdb7
s-top <15634647@qq.com>
2018-03-28 19:07:10 +0800

    2018-03-2

--- Java/CodingTest/test.java
+++ Java/CodingTest/test.java
@@ -1,8 +1,34 @@
 package com.lzp.Demo.NiuKeWang;
 
+链接：https://www.nowcoder.com/questionTerminal/1624bc35a45c42c0bc17d17fa0cba788
+来源：牛客网
+
 import java.util.*;
-
-public class Main {
-
-        adbc
+/**
+用一个双端队列，队列第一个位置保存当前窗口的最大值，当窗口滑动一次
+1.判断当前最大值是否过期
+2.新增加的值从队尾开始比较，把所有比他小的值丢掉
+*/
+public class Solution {
+   public ArrayList<Integer> maxInWindows(int [] num, int size)
+    {
+        ArrayList<Integer> res = new ArrayList<>();
+        if(size == 0) return res;
+        int begin; 
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        for(int i = 0; i < num.length; i++){
+            begin = i - size + 1;
+            if(q.isEmpty())
+                q.add(i);
+            else if(begin > q.peekFirst())
+                q.pollFirst();
+         
+            while((!q.isEmpty()) && num[q.peekLast()] <= num[i])
+                q.pollLast();
+            q.add(i);  
+            if(begin >= 0)
+                res.add(num[q.peekFirst()]);
+        }
+        return res;
+    }
 }
