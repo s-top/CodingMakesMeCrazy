a70947150607d1721bbb939eb1be3bcb9264321e
s-top <15634647@qq.com>
2018-04-09 20:33:15 +0800

    2018-04-09

--- Java/CodingTest/test.java
+++ Java/CodingTest/test.java
@@ -2,31 +2,85 @@ import java.util.Scanner;
 
 public class Main {
 
+
+    static String hws = "";
+    static int num = 0;
+    static String[] hw;
+
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		long[] nArr=new long[n];
-		for(int i=0;i<n;i++){
-			nArr[i]=sc.nextLong();
-		}
-		for(int i=0;i<n;i++){
-			long number=nArr[i];
-			long x=number,y=1;
-
-
-			while(x%2==0){
-				x=x/2;
-				y=y*2;
-			}
-			if(y==1){
-				System.out.println("No");
-			}else{
-				System.out.println(x+" "+y);
-			}
-
-		}
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        char[] c = s.toCharArray();
+        System.out.println("--------输出所有的回文------");
+        outPalindrome(c);
 
 	}
+
+	public static String[] createHw() {
+        return new String[num];
+    }
+
+    public static void hwsClose() {
+         hws = "";
+    }
+
+    public static int judgeNumber(char[] c) {
+        for (int i = 0; i < c.length - 1; i++) {
+            if (c[i] == c[i + 1]) {
+                num++;
+            }
+        }
+        return num;
+    }
+
+    public static void judge(char[] c) {
+        judgeNumber(c);
+        if (num != 0) {
+            hw = createHw();
+            for (int i = 0; i < c.length - 1; i++) {
+                if (c[i] == c[i + 1]) {
+                    hws = hws + c[i];
+                    judge2(c, i, i + 1);
+                    hw[--num] = hws;
+                    hwsClose();
+                }
+            }
+        } else {
+            System.out.println("该字符串没有回文");
+        }
+    }
+
+    public static void judge2(char[] c, int i, int k) {
+        if (i - 1 >= 0 && k + 1 < c.length) {
+            if (c[i - 1] == c[k + 1]) {
+                hws = hws + c[i - 1];
+                judge2(c, i - 1, k + 1);
+            }
+        }
+    }
+
+    public static void outPalindrome(char[] c) {
+         judge(c);
+         if (hw!=null) {
+             for (int i = 0; i < hw.length; i++) {
+                 System.out.println(reverse(hw[i])+hw[i]);
+             }
+         } else {
+             System.out.println("没有回文2");
+         }
+    }
+
+    public static StringBuffer reverse(String string){
+         StringBuffer sb=new StringBuffer();
+         char a;
+         for(int i=0;i<string.length();i++){
+             a=string.charAt(string.length()-1-i);
+             sb.append(a);
+         }
+         return sb;
+    }
+
+
 
 }
