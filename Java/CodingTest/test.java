e4b8f7aa9946a313c1a6cee11f7b95c76434065d
s-top <15634647@qq.com>
2018-04-18 20:55:46 +0800

    2018-04-18

--- Java/CodingTest/test.java
+++ Java/CodingTest/test.java
@@ -1,86 +1,47 @@
 import java.util.Scanner;
 
 public class Main {
+    import java.util.Scanner;
 
-
-    static String hws = "";
-    static int num = 0;
-    static String[] hw;
-
+public class Main {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int years = sc.nextInt();
+		int weeks = sc.nextInt();
+		if(years<=0||weeks<0||weeks>6||years>400){
+			System.out.println(-1);
+			return;
+		}
+		int[] days = {31,31,28,31,30,31,30,31,31,30,31,30};
+		int today = (13-31)%7;
+		int thisYear = 1900;
+		int count=0;
 
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        char[] c = s.toCharArray();
-        System.out.println("--------输出所有的回文------");
-        outPalindrome(c);
+		for(int i=0;i<years;i++){
+			boolean isR = false;
+			if(thisYear%100==0){
+				if(thisYear%400==0){
+					isR=true;
+				}
+			}else if(thisYear%4==0){
+				isR=true;
+			}
+			thisYear++;
+			for(int j=0;j<12;j++){
+				today = (today+days[j])%7;
+				if(j==2&&isR){
+					today++;
+					today=today%7;
+				}
+				if(today==weeks){
+					count++;
+				}
+			}
+		}
+
+		System.out.println(count);
 
 	}
 
-	public static String[] createHw() {
-        return new String[num];
-    }
-
-    public static void hwsClose() {
-         hws = "";
-    }
-
-    public static int judgeNumber(char[] c) {
-        for (int i = 0; i < c.length - 1; i++) {
-            if (c[i] == c[i + 1]) {
-                num++;
-            }
-        }
-        return num;
-    }
-
-    public static void judge(char[] c) {
-        judgeNumber(c);
-        if (num != 0) {
-            hw = createHw();
-            for (int i = 0; i < c.length - 1; i++) {
-                if (c[i] == c[i + 1]) {
-                    hws = hws + c[i];
-                    judge2(c, i, i + 1);
-                    hw[--num] = hws;
-                    hwsClose();
-                }
-            }
-        } else {
-            System.out.println("该字符串没有回文");
-        }
-    }
-
-    public static void judge2(char[] c, int i, int k) {
-        if (i - 1 >= 0 && k + 1 < c.length) {
-            if (c[i - 1] == c[k + 1]) {
-                hws = hws + c[i - 1];
-                judge2(c, i - 1, k + 1);
-            }
-        }
-    }
-
-    public static void outPalindrome(char[] c) {
-         judge(c);
-         if (hw!=null) {
-             for (int i = 0; i < hw.length; i++) {
-                 System.out.println(reverse(hw[i])+hw[i]);
-             }
-         } else {
-             System.out.println("没有回文2");
-         }
-    }
-
-    public static StringBuffer reverse(String string){
-         StringBuffer sb=new StringBuffer();
-         char a;
-         for(int i=0;i<string.length();i++){
-             a=string.charAt(string.length()-1-i);
-             sb.append(a);
-         }
-         return sb;
-    }
-
-
-
+}
 }
